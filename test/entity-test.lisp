17d75bd3453772934dea2643ad7c424badff3192
Jim Barrows <Jim.Barrows@gmail.com>
2018-03-23 17:11:15 -0500

    combining to different local directories.

--- test/entity-test.lisp
+++ test/entity-test.lisp
@@ -1,5 +1,4 @@
 (in-package :cl-ddd-test)
-
 (def-suite entity-test-suite :description "Entity tests")
 
 (in-suite entity-test-suite)
@@ -7,10 +6,6 @@
 (cl-ddd::defentity test-entity()
   ((slot1 )
    (slot2 )))
-
-(macroexpand-1 '(cl-ddd::defentity test-entity()
-  ((slot1 )
-   (slot2 ))))
 
 (defmacro with-repository (body)
   `(let ((repo (make-instance 'test-entity-repository))
@@ -38,7 +33,7 @@
 (test find-by-id-method-is-find-test-entity-by-id
   (is-true (find-method #'find-test-entity-by-id
 			'()
-			(mapcar #'find-class '(test-entity-repository test-entity))
+			(mapcar #'find-class '(test-entity-repository uuid))
 			nil)))
 
 (test list-data-method-is-list-test-entity
@@ -61,48 +56,48 @@
 
 (test can-add-entity-to-repository
       (with-repository
-	  ((add-entity repo test-entity-1)
-	   (is (= 1 (list-length (list-data repo)))))))
+	  ((add-test-entity repo test-entity-1)
+	   (is (= 1 (list-length (list-test-entity-data repo)))))))
 
 
 (test can-update-entity-in-repository
       (with-repository ((add-entity repo test-entity-1)
 			(setf (slot1 test-entity-1) "some value")
-			(is (string= "some value" (slot1 (first (list-data repo))))))))
+			(is (string= "some value" (slot1 (first (list-test-entity-data repo))))))))
 
 (test can-find-entity-by-id
       (with-repository (
-			(add-entity repo test-entity-1)
+			(add-test-entity repo test-entity-1)
 			(is (uuid= (id test-entity-1) 
-				   (id (find-by-id repo (id test-entity-1))))))))
+				   (id (find-test-entity-by-id repo (id test-entity-1))))))))
 
 (test can-delete-entity-from-repository
-      (with-repository ((add-entity repo test-entity-1)
-			(add-entity repo (make-instance 'test-entity))
-			(remove-entity repo test-entity-1)
-			(is (= 1 (list-length (list-data repo)))))))
+      (with-repository ((add-test-entity repo test-entity-1)
+			(add-test-entity repo (make-instance 'test-entity))
+			(remove-test-entity repo test-entity-1)
+			(is (= 1 (list-length (list-test-entity-data repo)))))))
 
 (test can-return-a-list-of-all-entities
-      (with-repository ((add-entity repo test-entity-1)
-			(add-entity repo (make-instance 'test-entity))
-			(is (= 2 (list-length (list-data repo)))))))
+      (with-repository ((add-test-entity repo test-entity-1)
+			(add-test-entity repo (make-instance 'test-entity))
+			(is (= 2 (list-length (list-test-entity-data repo)))))))
 
 (test can-load-entities-into-repository
-      (with-repository ((add-entity repo test-entity-1)
-			(add-entity repo (make-instance 'test-entity))
+      (with-repository ((add-test-entity repo test-entity-1)
+			(add-test-entity repo (make-instance 'test-entity))
 			(save-data repo)
 			(setf (slot-value repo 'cl-ddd::data) ())
 			(load-data repo)
-			(is (= 2 (list-length (list-data repo)))))))
+			(is (= 2 (list-length (list-test-entity-data repo)))))))
 
 (test can-save-entities-from-repository
-      (with-repository ((add-entity repo test-entity-1)
-			(add-entity repo (make-instance 'test-entity))
+      (with-repository ((add-test-entity repo test-entity-1)
+			(add-test-entity repo (make-instance 'test-entity))
 			(save-data repo)
 			(is-true (probe-file "TEST-ENTITY-REPOSITORY.data"))
 			(delete-file (probe-file "TEST-ENTITY-REPOSITORY.data")))))
 		    
 (test can-determine-if-entity-is-in-repo
-      (with-repository ((add-entity repo test-entity-1)
-			(is-true (entity-exists-? repo test-entity-1 )))))
+      (with-repository ((add-test-entity repo test-entity-1)
+			(is-true (test-entity-exists-? repo test-entity-1 )))))
 
